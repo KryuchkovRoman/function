da645db584cc5c1c87ea45e8d7eda0a6c62abc9e
KryuchkovRoman <114350287+KryuchkovRoman@users.noreply.github.com>
2023-04-08 21:23:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let a=document.querySelector('#inp1').value;
+let b=document.querySelector('#inp2').value;
+
+let button1=document.querySelector("#btn1");
+
+let txt=document.querySelector('#txt1');
+
+button1.addEventListener("click", ()=>{
+    c=Number(a)+Number(b);
+    console.log(c);
+    txt.innerHTML="Результат: "+(c) ;
+}
+)
+
+
+let button2=document.querySelector('#btn2');
+button2.addEventListener('click', addCircle);
+function addCircle(){
+    let newCircle=document.createElement('div');
+    tsk2.appendChild(newCircle);
+    newCircle.classList.add('circle');
+    
+}
+
+//task 3
+  let d=document.querySelector('#inp3').value;
+  let m=document.querySelector('#inp4').value;
+  let c=document.querySelector('#inp5').value;
+  let button3=document.querySelector("#btn3");
+  
+  let txt2=document.querySelector('#txt2');
+  
+  button3.addEventListener("click", ()=>{
+      g=Number(d)+Number(m)+Number(c);
+     console.log(g);
+      txt2.innerHTML="Результат: "+(g) ;
+  }
+  )
+
+  //task4
+  let button4=document.querySelector('#btn4');
+button4.addEventListener('click', addbox);
+function addbox(){
+    let newbox=document.createElement('div');
+    tsk4.appendChild(newbox);
+    newbox.classList.add('box');
+    
+}
