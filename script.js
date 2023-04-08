441d8e6e1ee065ff8bcb6b4ea734e8350f7205e3
KryuchkovRoman <114350287+KryuchkovRoman@users.noreply.github.com>
2023-04-08 21:35:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let a=document.querySelector('#inp1').value;
-let b=document.querySelector('#inp2').value;
-
 let button1=document.querySelector("#btn1");
-
 let txt=document.querySelector('#txt1');
 
 button1.addEventListener("click", ()=>{
+    let a=document.querySelector('#inp1').value;
+    let b=document.querySelector('#inp2').value;
     c=Number(a)+Number(b);
     console.log(c);
-    txt.innerHTML="Результат: "+(c) ;
+    txt.innerHTML="Результат: "+(c);
 }
 )
 
@@ -22,22 +20,19 @@ function addCircle(){
     
 }
 
-//task 3
-  let d=document.querySelector('#inp3').value;
-  let m=document.querySelector('#inp4').value;
-  let c=document.querySelector('#inp5').value;
+
   let button3=document.querySelector("#btn3");
-  
   let txt2=document.querySelector('#txt2');
-  
   button3.addEventListener("click", ()=>{
+      let d=document.querySelector('#inp3').value;
+        let m=document.querySelector('#inp4').value;
+       let c=document.querySelector('#inp5').value; 
       g=Number(d)+Number(m)+Number(c);
      console.log(g);
-      txt2.innerHTML="Результат: "+(g) ;
+      txt2.innerHTML="Результат: "+(g);
   }
   )
 
-  //task4
   let button4=document.querySelector('#btn4');
 button4.addEventListener('click', addbox);
 function addbox(){
